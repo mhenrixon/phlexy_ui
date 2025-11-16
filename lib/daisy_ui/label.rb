f4cb1d5b055b1b2e71dd5f4ff8ada4cd6042b9a5
mhenrixon <mikael@mhenrixon.com>
2025-11-16 19:01:42 +0100

    Fix label component

--- lib/daisy_ui/label.rb
+++ lib/daisy_ui/label.rb
@@ -1,15 +1,63 @@
 # frozen_string_literal: true
 
 module DaisyUI
+  # Label component for wrapping inputs, selects, or creating floating labels
+  #
+  # @example Floating label
+  #   DaisyUI::Label.new(:floating) do
+  #     input(type: "text", placeholder: "Email", class: "input input-md")
+  #     span { "Your Email" }
+  #   end
+  #
+  # @example Input wrapper with label
+  #   DaisyUI::Label.new(:input) do |label|
+  #     label.text("https://")
+  #     input(type: "text", placeholder: "URL")
+  #   end
+  #
+  # @example Select wrapper with label
+  #   DaisyUI::Label.new(:select) do |label|
+  #     label.text("Type")
+  #     select do
+  #       option { "Personal" }
+  #       option { "Business" }
+  #     end
+  #   end
   class Label < Base
-    self.component_class = :label
+    # No default component_class - it varies based on usage
+    # (floating-label, input, or select)
+    self.component_class = nil
+
+    register_modifiers(
+      # "sm:floating-label"
+      # "md:floating-label"
+      # "lg:floating-label"
+      floating: "floating-label",
+      # "sm:input"
+      # "md:input"
+      # "lg:input"
+      input: "input",
+      # "sm:select"
+      # "md:select"
+      # "lg:select"
+      select: "select"
+    )
 
     def view_template(&)
       label(class: classes, **attributes, &)
     end
 
-    def text(as: :span, **options, &)
-      public_send(as, class: component_classes("label-text", options: options), **options, &)
+    # Renders label text for input/select wrappers
+    # For floating labels, use a plain span instead
+    #
+    # @example
+    #   label.text("Username")
+    #   label.text { "Email Address" }
+    #   label.text("https://", class: "custom-class")
+    def text(content = nil, **options, &block)
+      span(class: component_classes("label", options: options), **options) do
+        content || yield
+      end
     end
   end
 end
--- spec/lib/daisy_ui/label_spec.rb
+++ spec/lib/daisy_ui/label_spec.rb
@@ -1,29 +1,203 @@
 require "spec_helper"
 
 describe DaisyUI::Label do
-  subject(:output) { render described_class.new }
-
-  describe "rendering a label" do
+  describe "floating label" do
     let(:component) do
       Class.new(Phlex::HTML) do
         def view_template(&)
-          render DaisyUI::Label.new do |label|
-            label.text class: "foo", data: {my: :foos} do
-              "Username"
+          render DaisyUI::Label.new(:floating) do
+            input(type: "text", placeholder: "Email", class: "input input-md")
+            span { "Your Email" }
+          end
+        end
+      end
+    end
+
+    subject(:output) { render component.new }
+
+    it "renders a floating label with input" do
+      expected_html = html <<~HTML
+        <label class="floating-label">
+          <input type="text" placeholder="Email" class="input input-md">
+          <span>Your Email</span>
+        </label>
+      HTML
+
+      is_expected.to eq(expected_html)
+    end
+  end
+
+  describe "input wrapper label" do
+    let(:component) do
+      Class.new(Phlex::HTML) do
+        def view_template(&)
+          render DaisyUI::Label.new(:input) do |label|
+            label.text("https://")
+            input(type: "text", placeholder: "URL")
+          end
+        end
+      end
+    end
+
+    subject(:output) { render component.new }
+
+    it "renders an input wrapper with label text" do
+      expected_html = html <<~HTML
+        <label class="input">
+          <span class="label">https://</span>
+          <input type="text" placeholder="URL">
+        </label>
+      HTML
+
+      is_expected.to eq(expected_html)
+    end
+  end
+
+  describe "input wrapper with label at the end" do
+    let(:component) do
+      Class.new(Phlex::HTML) do
+        def view_template(&)
+          render DaisyUI::Label.new(:input) do |label|
+            input(type: "text", placeholder: "domain name")
+            label.text(".com")
+          end
+        end
+      end
+    end
+
+    subject(:output) { render component.new }
+
+    it "renders an input wrapper with label text at the end" do
+      expected_html = html <<~HTML
+        <label class="input">
+          <input type="text" placeholder="domain name">
+          <span class="label">.com</span>
+        </label>
+      HTML
+
+      is_expected.to eq(expected_html)
+    end
+  end
+
+  describe "select wrapper label" do
+    let(:component) do
+      Class.new(Phlex::HTML) do
+        def view_template(&)
+          render DaisyUI::Label.new(:select) do |label|
+            label.text("Type")
+            select do
+              option { "Personal" }
+              option { "Business" }
             end
           end
         end
       end
     end
 
-    subject(:output) do
-      render component.new
+    subject(:output) { render component.new }
+
+    it "renders a select wrapper with label text" do
+      expected_html = html <<~HTML
+        <label class="select">
+          <span class="label">Type</span>
+          <select>
+            <option>Personal</option>
+            <option>Business</option>
+          </select>
+        </label>
+      HTML
+
+      is_expected.to eq(expected_html)
+    end
+  end
+
+  describe "floating label with different sizes" do
+    let(:component) do
+      Class.new(Phlex::HTML) do
+        def view_template(&)
+          render DaisyUI::Label.new(:floating) do
+            input(type: "text", placeholder: "Small", class: "input input-sm")
+            span { "Small" }
+          end
+          render DaisyUI::Label.new(:floating) do
+            input(type: "text", placeholder: "Medium", class: "input input-md")
+            span { "Medium" }
+          end
+          render DaisyUI::Label.new(:floating) do
+            input(type: "text", placeholder: "Large", class: "input input-lg")
+            span { "Large" }
+          end
+        end
+      end
     end
 
-    it "is expected to match the formatted HTML" do
+    subject(:output) { render component.new }
+
+    it "renders floating labels with different input sizes" do
       expected_html = html <<~HTML
-        <label class="label">
-          <span class="label-text foo" data-my="foos">Username</span>
+        <label class="floating-label">
+          <input type="text" placeholder="Small" class="input input-sm">
+          <span>Small</span>
+        </label>
+        <label class="floating-label">
+          <input type="text" placeholder="Medium" class="input input-md">
+          <span>Medium</span>
+        </label>
+        <label class="floating-label">
+          <input type="text" placeholder="Large" class="input input-lg">
+          <span>Large</span>
+        </label>
+      HTML
+
+      is_expected.to eq(expected_html)
+    end
+  end
+
+  describe "text helper with block" do
+    let(:component) do
+      Class.new(Phlex::HTML) do
+        def view_template(&)
+          render DaisyUI::Label.new(:input) do |label|
+            label.text { "Username" }
+            input(type: "text", placeholder: "Enter username")
+          end
+        end
+      end
+    end
+
+    subject(:output) { render component.new }
+
+    it "renders label text using a block" do
+      expected_html = html <<~HTML
+        <label class="input">
+          <span class="label">Username</span>
+          <input type="text" placeholder="Enter username">
+        </label>
+      HTML
+
+      is_expected.to eq(expected_html)
+    end
+  end
+
+  describe "text helper with custom attributes" do
+    let(:component) do
+      Class.new(Phlex::HTML) do
+        def view_template(&)
+          render DaisyUI::Label.new(:input) do |label|
+            label.text("https://", class: "custom-class", data: {test: "value"})
+            input(type: "text", placeholder: "URL")
+          end
+        end
+      end
+    end
+
+    subject(:output) { render component.new }
+
+    it "renders label text with custom attributes" do
+      expected_html = html <<~HTML
+        <label class="input">
+          <span class="label custom-class" data-test="value">https://</span>
+          <input type="text" placeholder="URL">
         </label>
       HTML
 
