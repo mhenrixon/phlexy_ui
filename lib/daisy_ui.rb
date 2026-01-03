1544cd6fc387d25e269b74656a83beeadd4b09ec
mhenrixon <mikael@mhenrixon.com>
2026-01-03 12:01:12 +0100

    docs: update README, gemspec URLs, and add gem entry point
    - Replace PhlexyUI references with DaisyUI throughout README
    - Update installation instructions for daisyui gem
    - Update gemspec URLs to point to mhenrixon/daisyui repo
    - Add lib/daisyui.rb entry point so `require "daisyui"` works
    - Configure Zeitwerk to ignore the entry point file
    - Fix file permissions on lib/daisyui.rb

--- lib/daisy_ui.rb
+++ lib/daisy_ui.rb
@@ -9,6 +9,7 @@ loader = Zeitwerk::Loader.for_gem
 loader.inflector.inflect(
   "daisy_ui" => "DaisyUI"
 )
+loader.ignore("#{__dir__}/daisyui.rb")
 loader.ignore("#{__dir__}/daisy_ui/updated_at.rb")
 loader.setup # ready!
 loader.load_file("#{__dir__}/daisy_ui/base.rb")
